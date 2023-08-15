7080be9280a7a3f764ff5d878a4e7ad09847cc5d
Vastagon <jacob@yanceytech.net>
2023-08-15 23:25:15 +0000

    Created cosmosdb database and container

--- main.tf
+++ main.tf
@@ -66,11 +66,73 @@ resource "azurerm_cdn_endpoint" "cdnendpoint" {
 
 
 
-# Create a virtual network
-# resource "azurerm_virtual_network" "vnet" {
-#  name                = "myTFVnet"
-#  address_space       = ["10.0.0.0/16"]
-#  location            = "westus2"
-#  resource_group_name = azurerm_resource_group.tf-website-rg.name
-# }
+# Create Resource Group for CosmosDB
+
+resource "azurerm_resource_group" "cosmosdbrg"{
+  name = "cosmosdbrg"
+  location = "eastus"
+}
+
+resource "azurerm_cosmosdb_account" "cosmosdb" {
+  name                = "vastagoncosmosdb"
+  location            = azurerm_resource_group.cosmosdbrg.location
+  resource_group_name = azurerm_resource_group.cosmosdbrg.name
+  offer_type          = "Standard"
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = "eastus"
+    failover_priority = 0
+  }
+  geo_location {
+    location          = "westus"
+    failover_priority = 1
+  }
+
+}
+
+resource "azurerm_cosmosdb_sql_database" "cosmossqldatabase" {
+  name                = "vastagonresumedb"
+  resource_group_name = azurerm_cosmosdb_account.cosmosdb.resource_group_name
+  account_name        = azurerm_cosmosdb_account.cosmosdb.name
+  throughput          = 400
+}
+
+resource "azurerm_cosmosdb_sql_container" "count-container" {
+  name                  = "count-container"
+  resource_group_name   = azurerm_cosmosdb_account.cosmosdb.resource_group_name
+  account_name          = azurerm_cosmosdb_account.cosmosdb.name
+  database_name         = azurerm_cosmosdb_sql_database.cosmossqldatabase.name
+  partition_key_path    = "/definition/id"
+  partition_key_version = 1
+  throughput            = 400
+
+  indexing_policy {
+    indexing_mode = "consistent"
+
+    included_path {
+      path = "/*"
+    }
+
+    included_path {
+      path = "/included/?"
+    }
+
+    excluded_path {
+      path = "/excluded/?"
+    }
+  }
+  unique_key {
+    paths = ["/definition/idlong", "/definition/idshort"]
+  }
+}
+
+
+
+
 
